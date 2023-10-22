8fd076d2b1647b050b2f4cca3f387321c78ed13a
tsubamenoki <144215790+tsubamenoki@users.noreply.github.com>
2023-10-22 01:49:40 +0000

    headerリンクまで

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  root to: "homes#top"
   devise_for :users
+  resources :users, only: [:index,:show,:edit,:update]
+  resources :books, only: [:index,:show,:create,:edit,:update,:destroy]
+  root to: "homes#top"
+  get '/homes/about'=>'homes#about', as: 'about'
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
