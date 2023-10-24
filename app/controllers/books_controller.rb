dad14979b33ca43d81e21602d5cf63395d69d74a
tsubamenoki <144215790+tsubamenoki@users.noreply.github.com>
2023-10-24 06:23:47 +0000

    edit制作途中

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,35 @@
 class BooksController < ApplicationController
   def index
+    @book=Book.new
+    @books=Book.all
+    @user=User.find(current_user.id)
+  end
+  
+  def create
+    @book=Book.new(book_params)
+    @book.user_id=current_user.id
+    @book.save
+    redirect_to books_path
   end
 
   def show
+    @book=Book.find(params[:id])
+    @user=User.find(current_user.id)
   end
 
   def edit
+    @book=Book.find(params[:id])
+  end
+  
+  def update
+  end
+  
+  def destroy
+  end
+  
+  private
+  
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,15 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
          
-  has_many :books, dependent: :destroy
+  has_one_attached :image
+  has_many :books, dependent: :destroy 
+  
+  def get_image
+    unless image.attached?
+      file_path = Rails.root.join('app/assets/images/no_image_yoko.jpg')
+      image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+    image
+  end
+  
 end
