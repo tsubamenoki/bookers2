cf197b559ac5c11fb49325cc9515abdd98f56654
tsubamenoki <144215790+tsubamenoki@users.noreply.github.com>
2023-10-25 06:46:17 +0000

    [finish]CRUD

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,7 +4,7 @@ class BooksController < ApplicationController
     @books=Book.all
     @user=User.find(current_user.id)
   end
-  
+
   def create
     @book=Book.new(book_params)
     @book.user_id=current_user.id
@@ -14,21 +14,26 @@ class BooksController < ApplicationController
 
   def show
     @book=Book.find(params[:id])
-    @user=User.find(current_user.id)
   end
 
   def edit
     @book=Book.find(params[:id])
   end
-  
+
   def update
+    @book=Book.find(params[:id])
+    @book.update(book_params)
+    redirect_to book_path(@book)
   end
-  
+
   def destroy
+    book=Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
   end
-  
+
   private
-  
+
   def book_params
     params.require(:book).permit(:title, :body)
   end
